8f9d9c792e6ae70fce895f16222f8ee92e97b1aa
JENILKHUNT99 <jenilkhunt22222@gmail.com>
2026-05-06 18:09:54 +0530

    Solve some typo error

--- init/init.sql
+++ init/init.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS dim_category (
 );
 
 CREATE TABLE IF NOT EXISTS dim_coin (
-    coin_id SERIAL PRIMARY KEY,
+    coin_id VARCHAR(20) PRIMARY KEY,
     coin_symbol VARCHAR(20) UNIQUE NOT NULL,
     coin_name VARCHAR(100) NOT NULL,
     category_id INTEGER NOT NULL,
@@ -12,29 +12,30 @@ CREATE TABLE IF NOT EXISTS dim_coin (
 );
 
 CREATE TABLE IF NOT EXISTS dim_date (
-    date_id SERIAL PRIMARY KEY,
-    full_date DATE UNIQUE NOT NULL,
+    date_id BIGINT PRIMARY KEY,
+    datetime TIMESTAMP UNIQUE NOT NULL,
+    full_date DATE NOT NULL,
     hour INTEGER NOT NULL,
     day INTEGER NOT NULL,
     day_name VARCHAR(20) NOT NULL,
     month INTEGER NOT NULL,
     month_name VARCHAR(20) NOT NULL,
-    quarter VARCHAR(5) NOT NULL,
+    quarter INTEGER NOT NULL,
     year INTEGER NOT NULL,
     is_weekend BOOLEAN NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS dim_currency (
-    currency_id SERIAL PRIMARY KEY,
+    currency_id INTEGER PRIMARY KEY,
     currency_name VARCHAR(50) NOT NULL,
     currency_symbol VARCHAR(10) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS fact_crypto_prices (
-    price_id SERIAL PRIMARY KEY,
-    coin_id INTEGER NOT NULL,
-    etl_run_date_id INTEGER NOT NULL,
-    api_updated_date_id INTEGER NOT NULL,
+    price_id VARCHAR(50) PRIMARY KEY,
+    coin_id VARCHAR(20) NOT NULL,
+    etl_run_date_id BIGINT NOT NULL,
+    api_updated_date_id BIGINT NOT NULL,
     currency_id INTEGER NOT NULL,
     price NUMERIC(20, 10) NOT NULL,
     market_cap NUMERIC(20, 2) NOT NULL,
